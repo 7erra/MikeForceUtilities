ebaa218841e9c330a850bd76a6c18e01111b1f69
7erra <38866692+7erra@users.noreply.github.com>
2021-05-14 12:34:41 +0200

    added logo

--- mod.cpp
+++ mod.cpp
@@ -1,3 +1,10 @@
 name = "Mike Force Utilities";
 dir = "@Mike Force Utilities";
 author = "Terra";
+logo = "logo_co.paa";
+logoActive = "logo_co.paa";
+tolltipOwned = "Mike Force Utilities";
+picture = "logo_co.paa";
+actionName = "GitHub";
+action = "https://github.com/7erra/MikeForceUtilities";
+overview = "Modding tools for the S.O.G. Prairie Fire CDLC gamemode Mike Force";
