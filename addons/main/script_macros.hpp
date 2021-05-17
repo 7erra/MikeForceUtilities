842a9e05919ec8fffd5ada86e1cca7ce785d717e
7erra <38866692+7erra@users.noreply.github.com>
2021-05-17 16:02:03 +0200

    PATH macro

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -1,1 +1,5 @@
 #define BREAKPOINT if (true) exitWith {nil}
+#define PATH(SUBPATH) QUOTE(\z\TER_MFU\addons\COMPONENT\SUBPATH)
+#ifdef DEBUG_MODE_FULL
+	#define PATH(SUBPATH) QUOTE(z\TER_MFU\addons\COMPONENT\SUBPATH)
+#endif
