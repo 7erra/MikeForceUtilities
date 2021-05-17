0979be751ccf4b323e707db3315681c056f7d55e
7erra <38866692+7erra@users.noreply.github.com>
2021-05-17 15:58:36 +0200

    added CfgFunctions (file)

---
+++ addons/ui/CfgFunctions.hpp
@@ -0,0 +1,13 @@
+class CfgFunctions
+{
+	class TER_MFU
+	{
+		class GUI
+		{
+			file = PATH(functions);
+			class displayLog;
+			class hashmapViewer;
+			class makeCtrlGroupMoveable;
+		};
+	};
+};
